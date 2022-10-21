1877808a2e770bb4cefd5eb6f655a64adad79891
Taavi Ansper <anspertaavi@gmail.com>
2022-10-21 12:44:41 +0300

    fixed routing errors

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import commentsRoutes from './components/comments/routes';
 import projectsRoutes from './components/projects/routes';
 import usersRoutes from './components/users/routes';
-import generalRoutes from './general/routes';
+import generalRoutes from './components/general/routes';
 import projectStatusesRoutes from './components/projectStatuses/routes';
 
 const app = express();
@@ -11,11 +11,11 @@ const path = '/api/v1';
 
 app.use(express.json());
 
-app.use(`${path}/health`, generalRoutes); //Serveri toimimise kontroll
-app.use(`${path}/users`, usersRoutes); // kasutaja
-app.use(`${path}/projectStatuses`, projectStatusesRoutes); // Projektistaatus
-app.use(`${path}/comments`, commentsRoutes); // kommentaarid
-app.use(`${path}/projects`, projectsRoutes); // projektid
+app.use(`${path}`, generalRoutes); //Serveri toimimise kontroll
+app.use(`${path}`, usersRoutes); // kasutaja
+app.use(`${path}`, projectStatusesRoutes); // Projektistaatus
+app.use(`${path}`, commentsRoutes); // kommentaarid
+app.use(`${path}`, projectsRoutes); // projektid
 
 app.listen(PORT, () => {
     console.log('Server is running');
